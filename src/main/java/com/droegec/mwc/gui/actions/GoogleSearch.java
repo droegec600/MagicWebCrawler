72cd0a2047d773f3b62615dc611c465c5eab1869
Christoph Droege <Christoph.Droege@akka.eu>
2020-01-15 16:24:23 +0100

    Initial Version 1.0

---
+++ src/main/java/com/droegec/mwc/gui/actions/GoogleSearch.java
@@ -0,0 +1,23 @@
+package com.droegec.mwc.gui.actions;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.ImageIcon;
+
+import com.droegec.mwc.gui.MainWindow;
+import com.droegec.mwc.gui.SearchWithGoogle;
+
+public class GoogleSearch extends AbstractSpiderAction {
+	private static final long serialVersionUID = -215590814971257589L;
+
+	public GoogleSearch(MainWindow wnd, String text) {
+		super(wnd, text, new ImageIcon(SearchNodeGoogleLinks.class.getResource("/images/find.png")));
+		putValue(SHORT_DESCRIPTION, "Search within this domain using Google index");
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (wnd.isThereSelectedNode())
+			new SearchWithGoogle(wnd, wnd.getSelectedNode().getURL().toString());
+	}
+
+}
